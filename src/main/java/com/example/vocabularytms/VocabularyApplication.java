71275f67b60faedbc9a592c319d7ca414c16768b
Polina <polinatrizna8@gmail.com>
2025-02-28 11:14:19 +0300

    set the vocabulary project

--- src/main/java/com/example/vocabularytms/VocabularyApplication.java
+++ src/main/java/com/example/vocabularytms/VocabularyApplication.java
@@ -10,17 +10,11 @@ import java.io.IOException;
 public class VocabularyApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // Надо прорисовать окно
-        // Создать окно
-        // Задать ему параметры
-        // Нанести на это окно объекты
-        // А потом все это сделать видимым - stage.show();
+
         FXMLLoader fxmlLoader = new FXMLLoader(VocabularyApplication.class.getResource("vocabularyTable.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 800);
         stage.setTitle("Vocabulary TMS");
         stage.setScene(scene);
-
-        // Не надо забывать сделать окно видимым
         stage.show();
         stage.setResizable(false);
     }
